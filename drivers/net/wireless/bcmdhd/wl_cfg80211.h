90ed7d4d8773113a5f850454a40a2aeb7f3f140e
binkybear <binkybear@nethunter.com>
2016-01-06 15:04:14 -0600

    Kernel oops fix
    Modified patch for https://github.com/CyanogenMod/android_kernel_samsung_d2/commit/88e9526f041871c3733d9549668efdc23ca7568c.
    
    Unable to test, but it appears that the Nexus 6P (specifically wireless bcmdhd) is trying to use notifier when new wireless device is connected.
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- drivers/net/wireless/bcmdhd/wl_cfg80211.h
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.h
@@ -590,6 +590,9 @@ struct bcm_cfg80211 {
 	s32 cfgdev_bssidx;
 	bool bss_pending_op;		/* indicate where there is a pending IF operation */
 	bool roam_offload;
+
+	struct notifier_block netdev_notifier;
+
 };
 
 
